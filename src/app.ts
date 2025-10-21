a3a0b2f667a219707744f023eefb1676e30066f9
khensin166 <khensin166@gmail.com>
2025-10-21 16:12:06 +0700

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 // app.ts
 import { Hono } from 'hono'
 import { authRoute } from './features/auth/presentation/authRoute'
+import { cors } from 'hono/cors'
 
 export const app = new Hono()
+
+// Hanya izinkan request dari frontend Vue Anda (default Vite)
+// 2. Gunakan middleware CORS (Izinkan semua)
+app.use('*', cors())
 
 app.route('/auth', authRoute)
 
