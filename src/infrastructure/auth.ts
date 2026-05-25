7ac6abec5817e69f653c1fda630cbf4d6dbd3640
Kenan Tomfie Bukit <119487201+khensin166@users.noreply.github.com>
2026-05-26 00:55:24 +0700

    [update] add credential google and github better oauth

--- src/infrastructure/auth.ts
+++ src/infrastructure/auth.ts
@@ -2,14 +2,27 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./database/prisma.js";
 
+const isProdOrStaging = process.env.NODE_ENV === 'production' || process.env.BETTER_AUTH_URL?.includes('kenantomfie.site');
+
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  basePath: "/auth",
   emailAndPassword: {
     enabled: true,
   },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+  },
   cookie: {
-    domain: ".kenantomfie.site",
+    domain: isProdOrStaging ? ".kenantomfie.site" : undefined,
   }
 });
